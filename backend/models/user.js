1a3c4c5d3f5488fa1ed7f1a0a00de12b6749bb96
stygian-96 <54054289+stygian-96@users.noreply.github.com>
2020-11-18 16:46:51 +0530

    Added user model

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+var userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    techStack: {
+        type: Array,
+        required: false,
+    }
+})
