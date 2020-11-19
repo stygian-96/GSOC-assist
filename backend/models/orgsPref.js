38e55990a3d90f23b3aa55baef4ea0d86401cdcf
stygian-96 <54054289+stygian-96@users.noreply.github.com>
2020-11-19 21:13:10 +0530

    Added orgsPref and projectsPref model

---
+++ backend/models/orgsPref.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const orgsSchema = new Schema({
+    org_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Organisation'
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+})
---
+++ backend/models/projectsPref.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const pojectsSchema = new Schema({
+    project_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Project'
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+})
