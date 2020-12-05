5051510eadb52d1631b6ba278b7d54b667079480
Adarsh Tripath <tripathiadarsh39@gmail.com>
2020-12-05 23:55:35 +0530

    added drawer

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,14 +1,95 @@
-import React from 'react'
-import '../Styles/Navbar.css'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const Navbar = () => {
-    return(
-        <div className = "navbar">
-            <div className = "logo">
-                Logo
-            </div>
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        padding: "0px "
+    },
+    title: {
+        flexGrow: 1,
+    },
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
+}));
+
+const Navbar = ()  =>  {
+    const classes = useStyles();
+
+    const [state,setState] = React.useState({
+        left: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <div
+            className={clsx(classes.list, {
+                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+            })}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Your Profile', 'Wishlist', 'LogOut'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
         </div>
-    )
+    );
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    {['left'].map((anchor) => (
+                        <React.Fragment key={anchor}>
+                            <Button onClick={toggleDrawer(anchor, true)} className={classes.menuButton}>
+                                <MenuIcon />
+                            </Button>
+                            <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+                                {list(anchor)}
+                            </Drawer>
+                        </React.Fragment>
+                    ))}
+                    <Typography variant="h6" className={classes.title}>
+                        GSOC-Assist
+          </Typography>
+                    <Button color="inherit">Year</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
+
 
 export default Navbar
