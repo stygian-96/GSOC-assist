e80c994dc8024a6ea1cad7df643bfc529786b7a4
Adarsh Tripathi <64785626+rush-tea@users.noreply.github.com>
2021-01-11 03:22:28 +0530

    Resolved merge conflict and added APIs in backend for github-oauth (#6)
    * added drawer
    
    * added orgs/project component
    
    * added reusable card component
    
    * added components
    
    * made backend APIs for github oauth login
    
    Co-authored-by: stygian-96 <54054289+stygian-96@users.noreply.github.com>

--- frontend/src/Pages/HomePage/Recommended-orgs.js
+++ frontend/src/Pages/HomePage/Recommended-orgs.js
@@ -2,7 +2,10 @@ import React from 'react';
 import StarIcon from '@material-ui/icons/Star';
 
 const RecommendedOrgs = () => {
+<<<<<<< HEAD
+=======
 
+>>>>>>> 62dda84872cea718cf0a50f38b3e73e88cccb1a9
     return(
         <div className="recommended-orgs-wrapper">
             <p>
