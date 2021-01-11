a9e68785c13090b681501cf01cdba181c52e4962
Adarsh Tripath <tripathiadarsh39@gmail.com>
2021-01-12 04:26:52 +0530

    added github-oauth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,23 @@ import HomePage from './Pages/HomePage/HomePage';
 import Login from './Pages/auth-pages/LoginPage';
 import SignUp from './Pages/auth-pages/SignUp';
 import WishListPage from './Pages/WishListPage/WishListPage';
+import {UserContextProvider} from './contexts/userContext';
+import Profile from './Pages/auth-pages/Profile';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route exact path='/login' component={Login} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/wishlist" component={WishListPage} />
-        </Switch>
-      </BrowserRouter>
+      <UserContextProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/home' component={HomePage} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/wishlist" component={WishListPage} />
+            <Route exact path="/profile" component={Profile} />
+          </Switch>
+        </BrowserRouter>
+      </UserContextProvider>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Router} from 'react-router';
 import reportWebVitals from './reportWebVitals';
+import history from './history';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
